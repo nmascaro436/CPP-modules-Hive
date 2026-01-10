5738f18c82ffb062452c48e012710de082197b41
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-01-10 11:49:43 +0200

    feat: working on ex01

---
+++ CPP01/ex01/Zombie.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {} 
+
+Zombie::~Zombie(){}
+
+void Zombie::announce(void)
+{
+	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+void Zombie::setName(std::string name)
+{
+	_name = name;
+}
