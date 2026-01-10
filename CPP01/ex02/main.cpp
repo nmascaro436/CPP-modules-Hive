95995608b9924fd83451e472b3074ebd01a4aa42
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-01-10 12:42:33 +0200

    feat: ex02 done

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -4,9 +4,21 @@
  * and its like a new variable. A POINTER POINTS TO AN OBJECT A REFERENCE IS THE OBJECT!
  */
 
-
+#include <iostream>
 
 int main ()
 {
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
 
+	std::cout << "Memory address of str: " << &str << std::endl;
+	std::cout << "Memory address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+
+	return 0;
 }
