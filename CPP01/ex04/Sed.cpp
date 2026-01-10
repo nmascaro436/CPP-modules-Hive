ec81233c0a5c60732f9188547438197be23e6de2
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-01-10 15:35:03 +0200

    feat: ex04 done

---
+++ CPP01/ex04/Sed.cpp
@@ -0,0 +1,32 @@
+#include "Sed.hpp"
+
+void replaceInFile(const std::string& filename, const std::string& s1, const std::string& s2)
+{
+	if (s1.empty())
+	{
+		std::cout << "Search string can't be empty." << std::endl;
+		return;
+	}
+	std::ifstream infile(filename); // open file for read only, file needs to exist already
+	std::ofstream outfile(std::string(filename) + ".replace"); // create file for writing
+	std::string line;
+
+	if (!infile || !outfile)
+	{
+		std::cout << "Can't open input file or create output file." << std::endl;
+		return ;
+	}
+
+	while (std::getline(infile, line)) // go line by line and store in "line"
+	{
+		size_t position = 0; // beginning of line
+		while ((position = line.find(s1, position)) != std::string::npos) // looks for s1 starting at index position, if found returns index, if not returns npos
+		{
+			line = line.substr(0, position) + s2 + line.substr(position + s1.length()); // copy until the position of s1, add s2 after it and copy what was after the s1
+			position += s2.length(); //move forward
+		}
+		outfile << line << '\n'; // write into outfile with a newline
+	}
+	infile.close();
+	outfile.close();
+}
---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,9 @@
+#include "Sed.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+		return 1;
+	replaceInFile(argv[1], argv[2], argv[3]);
+	return 0;
+}
