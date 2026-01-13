71986f0db173d90cb70b8a913b1265411da1abd7
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-01-13 13:34:25 +0200

    feat: ex00 done

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,11 +1,11 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(void): //members e.g. member1(initial_value), member2(initial_value)
+Fixed::Fixed(void): _fixedPointNumber(0)
 {
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other): //members e.g. member1(other.member1), member2(other.member2) 
+Fixed::Fixed(const Fixed &other): _fixedPointNumber(other._fixedPointNumber)
 {
     std::cout << "Copy constructor called" << std::endl;
 }
@@ -14,11 +14,21 @@ Fixed &Fixed::operator=(const Fixed &other)
 {
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &other)
-	// e.g., member1 = other.member1;
+	    _fixedPointNumber = other._fixedPointNumber;
     return (*this);
 }
 
 Fixed::~Fixed(void)
 {
     std::cout << "Destructor called" << std::endl;
+}
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return _fixedPointNumber;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+        _fixedPointNumber = raw;
 }
--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -5,7 +5,14 @@
  * - Copy assignment operator: copies the content of one object to another object that already exists
  * - Destructor: called when an object goes out of scope or is deleted, used for cleaning up resources
  * 
- * Operator overloading is basically defining what operators like +, =, - do with the custom classes
+ * Ad Hoc Polymorphism is a type of polymorphism where the same name (function or operator) can have
+ * different implementations depending on the types of its arguments.The appropriate implementation
+ * is selected by the compiler based on the argument types.
+ * 
+ * Fixed-point numbers are a way to represent decimal numbers using integers, where you reserve a fixed
+ * number of bits for the fractional part. Similar to converting euros to cents but using powers of 2.
+ * Most common one is 2^8 (256).
+ * 
  */
 
 #pragma once
@@ -15,10 +22,14 @@
 class Fixed
 {
 	private:
-		
+	int _fixedPointNumber;
+        static const int _fractionalBits = 8; // all the Fixed objects have this same value
+
 	public:
         Fixed(void);
         Fixed(const Fixed& other);
         Fixed &operator=(const Fixed &other);
         ~Fixed();
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
 };
