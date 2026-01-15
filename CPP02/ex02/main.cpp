964d6a07eb283bcf071fc7c76b43e344fbdcd95f
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-01-15 13:55:41 +0200

    feat: ex02 complete, added more tests

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -14,3 +14,26 @@ int main()
 	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
 }
+/*
+int main()
+{
+	Fixed x(5);
+	Fixed y(10);
+
+	// Test comparison operators
+	std::cout << (x < y) << std::endl;   // Should print 1 (true)
+	std::cout << (x > y) << std::endl;   // Should print 0 (false)
+	std::cout << (x == y) << std::endl;  // Should print 0 (false)
+
+	// Test addition/subtraction/division
+	std::cout << (x + y) << std::endl;   // Should print 15
+	std::cout << (y - x) << std::endl;   // Should print 5
+	std::cout << (y / x) << std::endl;   // Should print 2
+
+	// Test decrement
+	std::cout << --x << std::endl;
+	std::cout << x-- << std::endl;
+
+	// Test min
+	std::cout << Fixed::min(x, y) << std::endl;
+} */
