15e91256f6b45b1824ec3e82137455c6d93eb9f9
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-01-21 10:31:53 +0200

    feat: added comment

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -1,7 +1,7 @@
 /**
  * A reference works as an alias for an existing variable, providing an alternative name
  * for it and referring to the same memory. It´s not like a pointer that has its own memory
- * and its like a new variable. A POINTER POINTS TO AN OBJECT A REFERENCE IS THE OBJECT!
+ * and its like a new variable. A POINTER POINTS TO AN OBJECT, A REFERENCE IS THE OBJECT!
  */
 
 #include <iostream>
