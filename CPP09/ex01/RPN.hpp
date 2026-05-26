a2544a18c0561cb0904a99e6b28745fee4a58689
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-05-26 16:14:51 +0300

    feat: starting the 2nd exercise, set up the basics of the class

---
+++ CPP09/ex01/RPN.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <stack>
+
+/**
+ * RPN reads left to right, when you see a number you save it and when you see an operator 
+ * you use the last 2 saved numbers.
+ * Stack is best option here because it naturally keeps track of the last 2 numbers: we push
+ * numbers on top ad when we need 2 we just pop them off.
+ */
+
+
+
+// Push (add), top (read value), pop (remove)
+class RPN
+{
+	private:
+	std::stack<int> operation;
+
+
+
+
+	public:
+	RPN();
+	RPN(const RPN& other);
+	RPN& operator=(const RPN& other);
+	~RPN();
+} ;
