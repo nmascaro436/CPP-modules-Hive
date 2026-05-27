4704adc3356f17470c954927633f036a19b053b5
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-05-27 16:06:56 +0300

    feat: added function prototypes and explanation of the algorithm

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -1,2 +1,50 @@
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& other): _vec(other._vec), _deq(other._deq) {}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+	if (this != &other)
+	{
+		_vec = other._vec;
+		_deq = other._deq;
+	}
+	return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+void PmergeMe::parseInput(int argc, char **argv)
+{
+
+}
+
+void PmergeMe::vectorSort()
+{
+
+}
+
+void PmergeMe::dequeSort()
+{
+
+}
+
+
+void PmergeMe::FordJohnson(std::vector<int>& vec)
+{
+
+}
+void PmergeMe::FordJohnson(std::deque<int>& deq)
+{
+
+}
+void PmergeMe::binaryInsert(std::vector<int>& vec, int value)
+{
+
+}
+void PmergeMe::binaryInsert(std::deque<int>& deq, int value)
+{
+
+}
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -1,6 +1,43 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <deque>
+
+/**
+ * Ford-Johnson algorithm:
+ * - Pairs up numbers and compares each pair: get a "winner" (bigger num) and "loser" (smaller num)
+ * 		from each pair
+ * - Recursively sorts just the winners
+ * - Inserts the losers back into the sorted winners using binary search (uses Jacobsthal numbers
+ *  to minimize comparisons)
+ * 
+ * - Binary search always checks the middle of the already half sorted list to do less comparisons
+ * - Jacobsthal numbers give an order that keeps the search range as small as possible for
+ * 	each insertion
+ * 
+ * 
+ * 
+ * Vector stores elements contiguously in memory and has fast random access -> good for binary search
+ * Deque stores elements in chunks and has fast insertion at front and back -> good for building main chain
+ */
 
 class PmergeMe
 {
+	private:
+	std::vector<int> _vec;
+	std::deque<int>  _deq;
+	void FordJohnson(std::vector<int>& vec);
+	void FordJohnson(std::deque<int>& deq);
+	void binaryInsert(std::vector<int>& vec, int value);
+	void binaryInsert(std::deque<int>& deq, int value);
+
+	public:
+	PmergeMe();
+	PmergeMe(const PmergeMe& other);
+	PmergeMe& operator=(const PmergeMe& other);
+	~PmergeMe();
+	void parseInput(int argc, char **argv);
+	void vectorSort();
+	void dequeSort();
 
 };
